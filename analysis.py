42ccdb05e44d51df6992e9b53b7f1a66ebfce908
elweux <raghav.a@icloud.com>
2021-06-18 16:26:09 +0000

    modified the script

--- analysis.py
+++ analysis.py
@@ -15,6 +15,7 @@ class Mforensics:
         if check == 'y':
             dir = input("Enter the full path (where vol.py is located):")
             os.chdir(dir)
+            return dir
         
         #install volatility if user doesn't have it
         elif check == 'n':
@@ -24,6 +25,7 @@ class Mforensics:
             os.system("git clone https://github.com/volatilityfoundation/volatility3.git")
             os.chdir("volatility3")
             print("\n\nVolatility setup done!!")
+            return os.path.join(dir, "volatility3")
 
         else:
             sys.exit("Error, did you choose the correct option?")
@@ -31,10 +33,11 @@ class Mforensics:
 
     def menu(self):
         #MENU
-        print("\n\n**** MENU ****\n"\
+        print("\n**** MENU ****\n"\
               "1. OS Info\n"\
               "2. Hashes/Passwords\n"\
-              "3. Process List\n")
+              "3. Process List\n"\
+              "0. Quit\n")
 
         #TODO: implement control to make sure user put correct value
         option = int(input("Choose the option from menu: "))
@@ -42,22 +45,45 @@ class Mforensics:
         return option
 
 
-    def process(self, option):
-        check = os.path.exists("vol.py")
-        
-        if check == True:
-            if option == 1:
-                #get os information 
-                os.system(f"python3 vol.py -f {self.file} -q windows.info")
-            if option == 2:
+    def proc(self, option):
+        if option == 0:
+            sys.exit()
+
+        if option == 1:
+            #get os information 
+            os.system(f"python3 vol.py -f {self.file} -q windows.info")
+            
+        if option == 2:
+            print("********************\n"\
+                    "1. Hash Dump\n"\
+                    "2. Cache Dump\n"\
+                    "3. Lsa Dump\n")                       
+            alpha = int(input("Enter the choice: "))
+            if alpha == 1:
                 #grab common windows hashes (SAM+SYSTEM)
                 os.system(f"python3 vol.py -f {self.file} -q windows.hashdump")
-            if option == 3:
+            if alpha == 2:
+                #grab domain cache hashes inside the registery
+                os.system(f"python3 vol.py -f {self.file} -q windows.cachedump")
+            if alpha == 3:
+                #grab lsa secrets
+                os.system(f"python3 vol.py -f {self.file} -q windows.lsadump")
+
+        if option == 3:
+            print("********************\n"\
+                  "1. PsTree\n"\
+                  "2. PsList\n"\
+                  "3. PsScan\n")
+            alpha = int(input("Enter the choice: "))
+            if alpha == 1:
                 #get processes tree (won't get hidden processes)
                 os.system(f"python3 vol.py -f {self.file} -q windows.pstree")
-
-        else:
-            sys.exit("Check your path")
+            if alpha == 2:
+                #get process list (EPROCESSES)
+                os.system(f"python3 vol.py -f {self.file} -q windows.pslist")
+            if alpha == 3:
+                #get hidden process list (malware)
+                os.system(f"python3 vol.py -f {self.file} -q windows.psscan")
 
 
 if len(sys.argv) != 2:
@@ -67,7 +93,12 @@ file_path = os.path.realpath(sys.argv[1])
 
 m = Mforensics(file_path)
 
-m.req()
-option = m.menu()
-print()
-m.process(option)
+check = m.req()
+
+if os.path.exists("vol.py") is True: 
+    while True:
+        option = m.menu()
+        print()
+        m.proc(option)
+else:
+    sys.exit("\nError, vol.py is not found in the entered path.")
